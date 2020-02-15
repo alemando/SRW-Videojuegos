66161566c0daf503ca08e2c5856c0592fe9c4d6b
alemando <35750771+alemando@users.noreply.github.com>
2020-02-15 18:23:36 -0500

    Añadida ontologia integracion

--- src/videojuegos/Main.java
+++ src/videojuegos/Main.java
@@ -1,14 +1,27 @@
 package videojuegos;
 
+import java.io.FileNotFoundException;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		try {
+			new RDFEndPoint().consulta("SELECT DISTINCT * WHERE {\r\n" + 
+					"  ?s ?p ?o\r\n" + 
+					"}");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		new OWLVirtuosoEndPoint().consulta("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\r\n" + 
 				"PREFIX owl: <http://www.w3.org/2002/07/owl#>\r\n" + 
 				"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\r\n" + 
 				"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\r\n" + 
 				"SELECT ?subject ?object\r\n" + 
 				"	WHERE { ?subject rdfs:subClassOf ?object }");
+		
 		new D2RQEndPoint().consulta("SELECT DISTINCT * WHERE {\r\n" + 
 				"  ?s ?p ?o\r\n" + 
 				"}");
