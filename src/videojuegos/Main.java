e23de4f1c1a0c036b63b2b6d4344fd83ae00d01b
oyeu <daarangovi@gmail.com>
2020-02-16 11:47:35 -0500

    pull

--- src/videojuegos/Main.java
+++ src/videojuegos/Main.java
@@ -7,12 +7,12 @@ public class Main {
 				"PREFIX owl: <http://www.w3.org/2002/07/owl#>\r\n" + 
 				"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\r\n" + 
 				"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\r\n" + 
-				"SELECT ?subject ?object\r\n" + 
-				"	WHERE { ?subject rdfs:subClassOf ?object }");
-		new D2RQEndPoint().consulta("SELECT DISTINCT * WHERE {\r\n" + 
+				"SELECT *" + 
+				"from <Videojuegos>	WHERE { ?s ?p ?o }");
+		/*new D2RQEndPoint().consulta("SELECT DISTINCT * WHERE {\r\n" + 
 				"  ?s ?p ?o\r\n" + 
-				"}");
-		new DBPediaEndPoint().consulta("select distinct ?Concept where {[] a ?Concept} LIMIT 100");
+				"}");*/
+		//new DBPediaEndPoint().consulta("select distinct ?Concept where {[] a ?Concept} LIMIT 100");
 
 	}
 
