57ad92dba75965442d6daa45d0a9310832379ef4
Epalaciol <esteban.1054@hotmail.com>
2020-02-17 09:50:11 -0500

    Consulta en D2R yDbPedia

--- src/videojuegos/InterfazInstanciaIgual.java
+++ src/videojuegos/InterfazInstanciaIgual.java
@@ -4,12 +4,14 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
+import java.security.MessageDigest;
 import java.util.ArrayList;
 
 import javax.swing.*;
 
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
+
 
 
 public class InterfazInstanciaIgual extends Container implements ActionListener {
@@ -79,7 +81,20 @@ public class InterfazInstanciaIgual extends Container implements ActionListener 
 		else if(e.getActionCommand().equals("Volver")) {
 			Main.frame.setContentPane(new InterfazPrincipal());
 		}else if(e.getActionCommand().equals("Buscar")) {
-			buscarProperties(((ComboItem) jcmbEntidades.getSelectedItem()).getValue());
+			ArrayList<String> salidaFinal= consultaOntologias();
+			String salida ="";
+			for (int i= 0; i< salidaFinal.size();i++) {
+				salida += salidaFinal.get(i).toString();
+				salida +="\n";
+				
+			}
+			if(salida.isEmpty()) {
+				JOptionPane.showMessageDialog(getComponent(0), " No se encontraron coincidencias");
+				
+			}else {
+				
+				JOptionPane.showMessageDialog(getComponent(0), salida);
+			}
 		}
 		
 		
@@ -171,7 +186,6 @@ public class InterfazInstanciaIgual extends Container implements ActionListener 
 		ComboItem[] arreglo = new ComboItem[0];
 		ArrayList<QuerySolution> resultados = new OWLVirtuosoEndPoint().consulta("PREFIX vdo: <http://www.videogames.com/>\r\n"+
 				"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"+
-				//"PREFIX owl: <http://www.w3.org/2002/07/owl#>\r\n"+	
 				"SELECT DISTINCT ?individual\r\n" + 
 				"WHERE {\r\n" + 
 				"?individual rdf:type <"+entidad+">.\r\n"+
@@ -189,4 +203,137 @@ public class InterfazInstanciaIgual extends Container implements ActionListener 
 		return arreglo;
 
 	}
+	
+	public ArrayList<String> busqueda(){
+		String entidad = ((ComboItem) jcmbEntidades.getSelectedItem()).getValue();
+		String instancia = ((ComboItem) jcmbInstancias.getSelectedItem()).getValue();
+		String propiedad = ((ComboItem) jcmbProperties.getSelectedItem()).getValue();
+		String query = "PREFIX vdo: <http://www.videogames.com/>\r\n"+
+						"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\r\n"+
+						"SELECT DISTINCT ?individual ?found\r\n"+
+						"WHERE {\r\n"+
+							"?individual  rdf:type <"+ entidad +">.\r\n"+
+							"?individual  <"+ propiedad +"> ?found.\r\n"+
+						"}";
+		ArrayList<QuerySolution> busquedaOWL = new OWLVirtuosoEndPoint().consulta(query);
+		ArrayList<String> arreglo = new ArrayList<String>();
+		for(int i = 0; i < busquedaOWL.size(); i++) {
+			
+			String clase = busquedaOWL.get(i).get("?individual").toString();
+			if(clase.equals(instancia)) {
+				String valor = busquedaOWL.get(i).get("?found").toString();
+				arreglo.add(valor);
+				
+			}
+		}
+		for( int j= 0; j< arreglo.size(); j++) {
+		}
+		return arreglo;
+		
+	}
+	
+	public ArrayList<String> buscarClassEquivalents(String entidad) {
+		ArrayList<String> arreglo = new ArrayList<String>();
+		try {
+			
+			ArrayList<QuerySolution> resultados = new RDFIntegracionEndPoint().consulta("PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"+
+					"PREFIX owl: <http://www.w3.org/2002/07/owl#>\r\n"+
+					"SELECT DISTINCT ?class\r\n" + 
+					"WHERE {?class a owl:Class.\n" + 
+					" ?class owl:equivalentClass <"+ entidad +">."+
+					"} ");
+			for (int i = 0; i < resultados.size(); i++) {
+				
+				String item = resultados.get(i).get("?class").toString();
+				
+				arreglo.add(item);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return arreglo;
+	}
+	
+	public ArrayList<String> buscarPropertyEquivalents(String entidad, String propiedad) {
+		ArrayList<String> arreglo = new ArrayList<String>();
+		try {
+			ArrayList<QuerySolution> resultados = new RDFIntegracionEndPoint().consulta("PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"+
+					"PREFIX owl: <http://www.w3.org/2002/07/owl#>\r\n"+
+					"SELECT DISTINCT ?property\r\n" + 
+					"WHERE {?property a owl:DatatypeProperty.\n" + 
+					" ?class owl:equivalentClass <"+ entidad +">."+
+					"  ?property rdfs:domain ?class.\r\n" +
+					" ?property owl:equivalentProperty <"+ propiedad +">."+
+					"} ");
+			for (int i = 0; i < resultados.size(); i++) {
+				
+				String item = resultados.get(i).get("?property").toString();
+				arreglo.add(item);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return arreglo;
+	}
+	
+	public ArrayList<String> consultaOntologias(){
+		ArrayList<String> consulta = new ArrayList<String>();
+		String entidad = ((ComboItem) jcmbEntidades.getSelectedItem()).getValue();
+		String instancia = ((ComboItem) jcmbInstancias.getSelectedItem()).getValue();
+		String propiedad = ((ComboItem) jcmbProperties.getSelectedItem()).getValue();
+		ArrayList<String> entidades = buscarClassEquivalents(entidad);
+		ArrayList<String> propiedades = buscarPropertyEquivalents(entidad, propiedad);
+		ArrayList<String> valores = busqueda();
+		String valor = valores.get(0);
+		for(int i = 0; i < entidades.size();i++) {
+			String itemE = entidades.get(i);
+			String itemP = propiedades.get(i);
+			System.out.println(itemE+" "+itemP);
+			
+			if(itemE.contains("http://localhost:2020/resource/vocab/") && itemP.contains("http://localhost:2020/resource/vocab/")) {
+				for(int k =0 ; k< valores.size();k++) {
+					
+				
+				String queryD2 = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\r\n" + 
+						"PREFIX owl: <http://www.w3.org/2002/07/owl#>\r\n" + 
+						"SELECT DISTINCT ?individual ?found\r\n"+
+						"WHERE {\r\n"+
+							"?individual  a <"+ itemE +">.\r\n"+
+							"?individual  <"+ itemP +"> ?found.\r\n"+
+							"FILTER(?found='"+valores.get(k)+"')\r\n"+
+						"}";
+				ArrayList<QuerySolution> resultadosD2 = new D2RQEndPoint().consulta(queryD2);
+				for(int j=0; j< resultadosD2.size(); j++) {
+					consulta.add((resultadosD2.get(j).get("?individual")).toString());
+	
+				}
+				}
+			}else if(itemE.contains("http://dbpedia.org/ontology/") && itemP.contains("http://dbpedia.org/ontology/")) {
+				for(int k =0 ; k< valores.size();k++) {
+					
+					
+					String queryD2 = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\r\n" + 
+							"PREFIX owl: <http://www.w3.org/2002/07/owl#>\r\n" + 
+							"SELECT DISTINCT ?individual ?found\r\n"+
+							"WHERE {\r\n"+
+								"?individual  a <"+ itemE +">.\r\n"+
+								"?individual  <"+ itemP +"> ?found.\r\n"+
+								"FILTER(?found='"+valores.get(k)+"')\r\n"+
+							"}";
+					ArrayList<QuerySolution> resultadosDB = new DBPediaEndPoint().consulta(queryD2);
+					for(int j=0; j< resultadosDB.size(); j++) {
+						consulta.add((resultadosDB.get(j).get("?individual")).toString());
+		
+					}
+					}
+				
+			}
+		}	
+		
+		return consulta;
+		}
 }
